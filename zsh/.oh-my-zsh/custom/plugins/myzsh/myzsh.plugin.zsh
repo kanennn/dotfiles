0fd4a1ec31a6ca2cccac9a2415ed0025266776b9
Kanen Stephens <kanenstephens@icloud.com>
2026-01-03 13:54:43 -0600

    Added a stub zsh config

---
+++ zsh/.oh-my-zsh/custom/plugins/myzsh/myzsh.plugin.zsh
@@ -0,0 +1,15 @@
+# Lines configured by zsh-newuser-install
+HISTFILE=~/.histfile
+HISTSIZE=1000
+SAVEHIST=1000
+setopt notify
+setopt globdots
+unsetopt autocd beep extendedglob
+bindkey -v
+# End of lines configured by zsh-newuser-install
+# The following lines were added by compinstall
+zstyle :compinstall filename '/home/kanennn/.zshrc'
+
+autoload -Uz compinit
+compinit
+# End of lines added by compinstall
